4db1ce5039bd1115cc03c0d3410d7b573091f474
Jaguar Zhou <ai.ourship@gmail.com>
2025-07-04 21:51:59 +0800

    feat: allow clear config

--- apps/extension/src/entrypoints/options/components/KanjiFilterDashboard.tsx
+++ apps/extension/src/entrypoints/options/components/KanjiFilterDashboard.tsx
@@ -20,8 +20,8 @@ export default function KanjiFilterDashboard({
   onChange,
 }: KanjiFilterDashboardProps) {
   const { t } = useTranslation();
-  const [resetRuleDialogIsOpen, setResetRuleDialogIsOpen] = useState(false);
 
+  const [resetRuleDialogIsOpen, setResetRuleDialogIsOpen] = useState(false);
   const resetConfig = async () => {
     const db = await getKanjiFilterDB();
     await db.clear(DB.onlyTable);
@@ -31,6 +31,14 @@ export default function KanjiFilterDashboard({
     }
     setResetRuleDialogIsOpen(false);
     onChange(defaultKanjiFilterRules);
+  };
+
+  const [clearRuleDialogIsOpen, setClearRuleDialogIsOpen] = useState(false);
+  const clearConfig = async () => {
+    const db = await getKanjiFilterDB();
+    await db.clear(DB.onlyTable);
+    setClearRuleDialogIsOpen(false);
+    onChange([]);
   };
 
   return (
@@ -47,6 +55,56 @@ export default function KanjiFilterDashboard({
             {t("btnAddRule")}
           </span>
         </button>
+        <button
+          onClick={() => {
+            setClearRuleDialogIsOpen(true);
+          }}
+          className="flex max-w-40 grow cursor-pointer items-center justify-center gap-1 rounded-md bg-slate-950/5 px-1.5 py-2 text-slate-800 transition hover:text-sky-500 sm:px-3 dark:bg-white/5 dark:text-white"
+        >
+          <i className="i-tabler-clear-all size-5" />
+          <span className="max-w-32 overflow-hidden overflow-ellipsis whitespace-nowrap">
+            Clear Config
+          </span>
+        </button>
+        <PopupTransition show={clearRuleDialogIsOpen}>
+          <Dialog
+            as="div"
+            className="-translate-x-1/2 -translate-y-1/2 fixed top-1/2 left-1/2 z-40 min-w-80"
+            onClose={() => {
+              setClearRuleDialogIsOpen(false);
+            }}
+          >
+            <DialogPanel className="w-full min-w-[20rem] max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all dark:bg-slate-900">
+              <DialogTitle
+                as="h3"
+                className="font-medium text-gray-900 text-lg leading-6 dark:text-white"
+              >
+                Are you sure you want to clear the rule list?
+              </DialogTitle>
+              <div className="mt-2">
+                <p className="whitespace-pre-wrap text-gray-500 text-sm dark:text-gray-400">
+                  {t("undoneDesc")}
+                </p>
+              </div>
+              <div className="mt-4 flex gap-2.5">
+                <button
+                  className="inline-flex cursor-pointer justify-center rounded-md border border-transparent bg-red-100 px-4 py-2 font-medium text-slate-900 text-sm transition hover:bg-red-200 focus:outline-hidden focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2 dark:bg-red-800 dark:text-slate-200 dark:hover:bg-red-900"
+                  onClick={clearConfig}
+                >
+                  Clear
+                </button>
+                <button
+                  className="inline-flex cursor-pointer justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 font-medium text-blue-900 text-sm transition hover:bg-blue-200 focus:outline-hidden focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 dark:bg-slate-700 dark:text-slate-200 dark:hover:bg-slate-800"
+                  onClick={() => {
+                    setClearRuleDialogIsOpen(false);
+                  }}
+                >
+                  {t("btnCancel")}
+                </button>
+              </div>
+            </DialogPanel>
+          </Dialog>
+        </PopupTransition>
         <button
           className="flex max-w-40 grow cursor-pointer items-center justify-center gap-1 rounded-md bg-slate-950/5 px-1.5 py-2 text-slate-800 transition hover:text-sky-500 sm:px-3 dark:bg-white/5 dark:text-white"
           onClick={() => {
