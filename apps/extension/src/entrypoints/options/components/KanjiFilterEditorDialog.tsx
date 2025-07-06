a873660c566479d2dca93b4ab1354fe0988de1c5
Jaguar Zhou <ai.ourship@gmail.com>
2025-07-06 18:51:48 +0800

    feat: verify that the input must be kanji and katakana

--- apps/extension/src/entrypoints/options/components/KanjiFilterEditorDialog.tsx
+++ apps/extension/src/entrypoints/options/components/KanjiFilterEditorDialog.tsx
@@ -1,5 +1,5 @@
 import type { FilterRule } from "@/commons/constants";
-import { cn } from "@/commons/utils";
+import { DB, cn, getKanjiFilterDB } from "@/commons/utils";
 import {
   Dialog,
   DialogPanel,
@@ -13,6 +13,8 @@ import {
 } from "@headlessui/react";
 import { useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
+import { isKanji, isKatakana } from "wanakana";
+
 import PopupTransition from "./PopupTransition";
 
 interface KanjiFilterEditorDialogProps {
@@ -31,6 +33,47 @@ export default function KanjiFilterDialogEditor({
   const { t } = useTranslation();
   const [kanjiInput, setKanjiInput] = useState(rule.kanji);
   const [katakanasInput, setKatakanasInput] = useState(rule.katakanas);
+
+  const [kanjiInputErrorMessage, setKanjiInputErrorMessage] = useState("");
+  const [katakanasInputErrorMessage, setKatakanasInputErrorMessage] = useState("");
+
+  const validateInputs = async (filterRule: FilterRule) => {
+    setKanjiInputErrorMessage("");
+    setKatakanasInputErrorMessage("");
+
+    let hasError = false;
+
+    if (isKanji(filterRule.kanji)) {
+      const db = await getKanjiFilterDB();
+      const isDuplicate =
+        (await db.get(DB.onlyTable, filterRule.kanji)) && rule.kanji !== filterRule.kanji;
+      if (isDuplicate) {
+        setKanjiInputErrorMessage("This kanji is already in use.");
+        hasError = true;
+      }
+    } else if (filterRule.kanji.length > 0) {
+      setKanjiInputErrorMessage("Must be pure Japanese kanji.");
+      hasError = true;
+    }
+
+    for (const input of rule.katakanas) {
+      if (!isKatakana(input)) {
+        setKatakanasInputErrorMessage("Must be pure katakana.");
+        hasError = true;
+        break;
+      }
+    }
+
+    return !hasError;
+  };
+
+  const handleSubmit = async () => {
+    const valid = await validateInputs({ kanji: kanjiInput, katakanas: katakanasInput });
+    if (valid) {
+      onConfirm({ kanji: kanjiInput, katakanas: katakanasInput });
+    }
+  };
+
   return (
     <PopupTransition show={open}>
       <Dialog
@@ -74,38 +117,54 @@ export default function KanjiFilterDialogEditor({
               Edit your kanji filter
             </DialogTitle>
             <div className="mt-10 space-y-6 sm:mx-auto sm:w-full sm:max-w-sm">
-              <Field>
+              <Field className="relative">
                 <Label className="font-medium text-slate-950 text-sm/6 after:ml-0.5 after:text-red-500 after:content-['*'] dark:text-white">
                   Kanji
                 </Label>
                 <Input
                   value={kanjiInput}
-                  onChange={(e) => setKanjiInput(e.target.value.trim())}
+                  onChange={(e) => {
+                    setKanjiInput(e.target.value.trim());
+                    validateInputs({ kanji: e.target.value.trim(), katakanas: katakanasInput });
+                  }}
                   placeholder="漢字"
                   autoFocus={true}
                   className={
                     "mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-xs ring-1 ring-gray-300 ring-inset placeholder:text-gray-400 focus:ring-2 focus:ring-sky-600 focus:ring-inset disabled:cursor-not-allowed sm:text-sm sm:leading-6 dark:bg-slate-900 dark:text-white dark:ring-gray-700 dark:focus:ring-sky-600"
                   }
                 />
+                <p className="-bottom-5 absolute left-0 text-red-500">{kanjiInputErrorMessage}</p>
               </Field>
-              <Field>
+              <Field className="relative">
                 <Label className="font-medium text-slate-950 text-sm/6 after:ml-0.5 after:text-red-500 after:content-['*'] dark:text-white">
                   Yomikata
                 </Label>
                 <KatakanaTagsAdder
                   className="mt-2"
                   katakanas={katakanasInput}
-                  onChange={setKatakanasInput}
+                  onChange={(newKatakanas) => {
+                    setKatakanasInput(newKatakanas);
+                    validateInputs({
+                      kanji: kanjiInput,
+                      katakanas: newKatakanas,
+                    });
+                  }}
                 />
+                <p className="-bottom-5 absolute left-0 text-red-500">
+                  {katakanasInputErrorMessage}
+                </p>
               </Field>
               <div className="mt-4 flex gap-2.5">
                 <button
                   type="button"
                   className="flex w-full cursor-pointer justify-center rounded-md bg-sky-600 px-3 py-1.5 font-semibold text-sm text-white leading-6 shadow-xs focus-visible:outline-2 focus-visible:outline-sky-600 focus-visible:outline-offset-2 enabled:hover:bg-sky-500 disabled:cursor-not-allowed disabled:opacity-50"
-                  onClick={() => {
-                    onConfirm({ kanji: kanjiInput, katakanas: katakanasInput });
-                  }}
-                  disabled={!kanjiInput || katakanasInput.length === 0}
+                  onClick={handleSubmit}
+                  disabled={
+                    !kanjiInput ||
+                    katakanasInput.length === 0 ||
+                    !!kanjiInputErrorMessage ||
+                    !!katakanasInputErrorMessage
+                  }
                 >
                   {t("submit")}
                 </button>
@@ -137,12 +196,15 @@ const KatakanaTagsAdder = ({ katakanas, className, onChange }: KatakanaTagsAdder
 
   const inputRef = useRef<HTMLInputElement>(null);
   return (
-    <div
+    <button
       className={cn(
-        "flex w-full flex-wrap gap-1.5 rounded-md border-0 px-1 py-1.5 shadow-xs ring-1 ring-gray-300 ring-inset focus:ring-2 focus:ring-sky-600 focus:ring-inset disabled:cursor-not-allowed sm:text-sm sm:leading-6 dark:bg-slate-900 dark:ring-gray-700 dark:focus:ring-sky-600",
+        "flex w-full cursor-text flex-wrap gap-1.5 rounded-md border-0 px-1 py-1.5 shadow-xs ring-1 ring-gray-300 ring-inset focus:ring-2 focus:ring-sky-600 focus:ring-inset disabled:cursor-not-allowed sm:text-sm sm:leading-6 dark:bg-slate-900 dark:ring-gray-700 dark:focus:ring-sky-600",
         "has-focus:ring-2 has-focus:ring-sky-600 has-focus:ring-inset dark:has-focus:ring-sky-600",
         className,
       )}
+      onClick={() => {
+        setInputting(true);
+      }}
     >
       {katakanas.map((katakana) => {
         return (
@@ -163,7 +225,39 @@ const KatakanaTagsAdder = ({ katakanas, className, onChange }: KatakanaTagsAdder
           </div>
         );
       })}
-      {!inputting && (
+      {inputting ? (
+        <Input
+          value={inputValue}
+          ref={inputRef}
+          onFocus={() => setInputting(true)}
+          onChange={(e) => setInputValue(e.target.value)}
+          autoFocus
+          onKeyDown={(e) => {
+            if (e.key === "Escape") {
+              e.preventDefault();
+              setInputting(false);
+              setInputValue("");
+              if (inputRef.current) {
+                inputRef.current.blur();
+              }
+            }
+
+            if (e.key === "Enter") {
+              handleInputConfirm();
+            }
+          }}
+          onBlur={() => {
+            handleInputConfirm();
+            setInputting(false);
+          }}
+          placeholder={inputting ? "カタカナ" : ""}
+          type="text"
+          className={cn(
+            "min-w-8 flex-1 border-0 px-2 py-0 text-gray-900 ring-0 sm:text-sm sm:leading-6 dark:bg-slate-900 dark:text-white",
+            katakanas.length > 0 && "p-0",
+          )}
+        />
+      ) : (
         <button
           type="button"
           onClick={() => {
@@ -176,36 +270,6 @@ const KatakanaTagsAdder = ({ katakanas, className, onChange }: KatakanaTagsAdder
           <span className="sr-only">Add Katakana</span>
         </button>
       )}
-      <Input
-        value={inputValue}
-        ref={inputRef}
-        onFocus={() => setInputting(true)}
-        onChange={(e) => setInputValue(e.target.value)}
-        onKeyDown={(e) => {
-          if (e.key === "Escape") {
-            e.preventDefault();
-            setInputting(false);
-            setInputValue("");
-            if (inputRef.current) {
-              inputRef.current.blur();
-            }
-          }
-
-          if (e.key === "Enter") {
-            handleInputConfirm();
-          }
-        }}
-        onBlur={() => {
-          handleInputConfirm();
-          setInputting(false);
-        }}
-        placeholder={inputting ? "カタカナ" : ""}
-        type="text"
-        className={cn(
-          "min-w-8 flex-1 border-0 px-2 py-0 text-gray-900 ring-0 sm:text-sm sm:leading-6 dark:bg-slate-900 dark:text-white",
-          katakanas.length > 0 && "p-0",
-        )}
-      />
-    </div>
+    </button>
   );
 };
