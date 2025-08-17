f01439deec30fd68572a2436c6579b0d902d12a9
Jaguar Zhou <ai.ourship@gmail.com>
2025-08-17 19:43:01 +0800

    feat: allows editing of the domain field of an existing rule

--- apps/extension/src/entrypoints/options/routes/Selector/components/SelectorRuleEditorDialog.tsx
+++ apps/extension/src/entrypoints/options/routes/Selector/components/SelectorRuleEditorDialog.tsx
@@ -1,16 +1,21 @@
 import {
   Dialog,
   DialogPanel,
+  DialogTitle,
   Disclosure,
   DisclosureButton,
   DisclosurePanel,
+  Field,
+  Input,
+  Label,
 } from "@headlessui/react";
+import { trimStart } from "es-toolkit";
 import { useState } from "react";
 import { Trans, useTranslation } from "react-i18next";
-
 import type { SelectorRule } from "@/commons/constants";
 import { cn } from "@/commons/utils";
 import { PopupTransition } from "@/entrypoints/options/components/PopupTransition";
+import { useSelectorsStore } from "../store";
 
 type SelectorRuleEditorDialogProps = UpdateProps | CreateProps;
 
@@ -31,20 +36,59 @@ interface CreateProps {
 
 export function SelectorRuleEditorDialog(props: SelectorRuleEditorDialogProps) {
   const { mode, open, onClose } = props;
-  const [domain, setDomain] = useState(mode === "create" ? "" : props.originalRule.domain);
-  const [selector, setSelector] = useState(mode === "create" ? "" : props.originalRule.selector);
+  const selectorRules = useSelectorsStore((state) => state.selectors);
+  const [domainInput, setDomainInput] = useState(
+    mode === "create" ? "" : props.originalRule.domain,
+  );
+  const [selectorInput, setSelectorInput] = useState(
+    mode === "create" ? "" : props.originalRule.selector,
+  );
   const active = mode === "create" ? true : props.originalRule.active;
 
-  function submit(event: React.MouseEvent<HTMLButtonElement>) {
-    event.preventDefault();
-    if (mode === "create") {
-      props.onCreate({ domain, selector, active });
-    } else {
-      props.onUpdate({ domain, selector, active }, props.originalRule);
+  const { t } = useTranslation();
+
+  const [domainInputErrorMessage, setDomainInputErrorMessage] = useState("");
+  const [selectorInputErrorMessage, setSelectorInputErrorMessage] = useState("");
+  const validateDomainInput = (domain: string) => {
+    setDomainInputErrorMessage("");
+    if (domain.length === 0) {
+      setDomainInputErrorMessage(t("validationRequired"));
+      return false;
+    }
+
+    const selectorIsDuplicated = selectorRules.some((rule) => rule.domain === domain);
+    if (
+      (mode === "create" && selectorIsDuplicated) ||
+      (mode === "update" && domain !== props.originalRule.domain && selectorIsDuplicated)
+    ) {
+      setDomainInputErrorMessage(t("validationNonRepetitiveDomain"));
+      return false;
+    }
+
+    return true;
+  };
+
+  const validateSelectorInput = (selector: string) => {
+    setSelectorInputErrorMessage("");
+    if (selector.length === 0) {
+      setSelectorInputErrorMessage(t("validationRequired"));
+      return false;
+    }
+    return true;
+  };
+
+  function handleSubmit() {
+    const valid = validateDomainInput(domainInput) && validateSelectorInput(selectorInput);
+    if (valid) {
+      const newRule = { domain: domainInput, selector: selectorInput, active };
+      if (mode === "create") {
+        props.onCreate(newRule);
+      } else {
+        props.onUpdate(newRule, props.originalRule);
+      }
+      onClose();
     }
   }
-
-  const { t } = useTranslation();
 
   return (
     <PopupTransition show={open}>
@@ -100,68 +144,82 @@ export function SelectorRuleEditorDialog(props: SelectorRuleEditorDialogProps) {
 
             <div className="flex min-h-full flex-col justify-center p-6 lg:px-8">
               <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                <h2 className="text-center font-bold text-2xl text-gray-900 leading-9 tracking-tight dark:text-white">
+                <DialogTitle
+                  as="h3"
+                  className="text-center font-bold text-2xl text-gray-900 leading-9 tracking-tight dark:text-white"
+                >
                   {t("titleEditSelectorDialog", {
                     verbs: mode === "update" ? t("update") : t("create"),
                   })}
-                </h2>
+                </DialogTitle>
               </div>
 
               <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                <form className="space-y-6">
-                  <div>
-                    <label
+                <div className="space-y-6">
+                  <Field className="relative">
+                    <Label
                       htmlFor="domain"
                       className="block font-medium text-gray-900 text-sm capitalize leading-6 before:mr-1 before:text-red-500 before:content-['*'] after:ml-0.5 dark:text-slate-200"
                     >
                       {t("fieldDomain")}
-                    </label>
+                    </Label>
                     <div className="mt-2">
-                      <input
+                      <Input
                         id="domain"
                         name="domain"
-                        disabled={mode === "update"}
                         required
                         placeholder="*.example.com"
                         className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-xs ring-1 ring-gray-300 ring-inset placeholder:text-gray-400 focus:ring-2 focus:ring-sky-600 focus:ring-inset disabled:cursor-not-allowed sm:text-sm sm:leading-6 dark:bg-slate-900 dark:text-white dark:ring-gray-700 dark:focus:ring-sky-600"
-                        value={domain}
-                        onChange={(e) => setDomain(e.target.value)}
+                        value={domainInput}
+                        onChange={(e) => {
+                          const value = e.target.value.trim().replace(/^https?:\/\//, "");
+                          setDomainInput(value);
+                          validateDomainInput(value);
+                        }}
                       />
                     </div>
-                  </div>
+                    <p className="-bottom-5 absolute left-0 text-red-500">
+                      {domainInputErrorMessage}
+                    </p>
+                  </Field>
 
-                  <div>
-                    <div className="flex items-center justify-between">
-                      <label
-                        htmlFor="selector"
-                        className="block font-medium text-gray-900 text-sm capitalize leading-6 before:mr-1 before:text-red-500 before:content-['*'] after:ml-0.5 dark:text-slate-200"
-                      >
-                        {t("fieldSelector")}
-                      </label>
-                    </div>
+                  <Field className="relative">
+                    <Label
+                      htmlFor="selector"
+                      className="block font-medium text-gray-900 text-sm capitalize leading-6 before:mr-1 before:text-red-500 before:content-['*'] after:ml-0.5 dark:text-slate-200"
+                    >
+                      {t("fieldSelector")}
+                    </Label>
                     <div className="mt-2">
-                      <input
+                      <Input
                         id="selector"
                         name="selector"
                         required
                         placeholder="body"
                         className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-xs ring-1 ring-gray-300 ring-inset placeholder:text-gray-400 focus:ring-2 focus:ring-sky-600 focus:ring-inset sm:text-sm sm:leading-6 dark:bg-slate-900 dark:text-white dark:ring-gray-700 dark:focus:ring-sky-600"
-                        value={selector}
-                        onChange={(e) => setSelector(e.target.value)}
+                        value={selectorInput}
+                        onChange={(e) => {
+                          const value = trimStart(e.target.value);
+                          setSelectorInput(value);
+                          validateSelectorInput(value);
+                        }}
                       />
                     </div>
-                  </div>
+                    <p className="-bottom-5 absolute left-0 text-red-500">
+                      {selectorInputErrorMessage}
+                    </p>
+                  </Field>
 
                   <div>
                     <button
                       className="flex w-full cursor-pointer justify-center rounded-md bg-sky-600 px-3 py-1.5 font-semibold text-sm text-white leading-6 shadow-xs focus-visible:outline-2 focus-visible:outline-sky-600 focus-visible:outline-offset-2 enabled:hover:bg-sky-500 disabled:cursor-not-allowed disabled:opacity-50"
-                      disabled={!(domain && selector)}
-                      onClick={submit}
+                      disabled={!!domainInputErrorMessage || !!selectorInputErrorMessage}
+                      onClick={handleSubmit}
                     >
                       {t("submit")}
                     </button>
                   </div>
-                </form>
+                </div>
               </div>
             </div>
           </div>
