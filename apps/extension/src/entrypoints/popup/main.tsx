190a814732d3c56cb86c48fe69ecbc25f8cdf8ce
Jaguar Zhou <ai.ourship@gmail.com>
2025-09-13 18:57:32 +0800

    feat: error tracking for options and popup pages

--- apps/extension/src/entrypoints/popup/main.tsx
+++ apps/extension/src/entrypoints/popup/main.tsx
@@ -1,3 +1,4 @@
+import * as Sentry from "@sentry/react";
 import { ThemeProvider } from "next-themes";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
@@ -6,6 +7,13 @@ import "@/tailwind.css";
 import "@/commons/i18n";
 
 import { Root } from "./root";
+
+Sentry.init({
+  dsn: "https://93bfaefbe956cf67fc9baad45d9c8357@o4507553809498112.ingest.us.sentry.io/4508510108319744",
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
